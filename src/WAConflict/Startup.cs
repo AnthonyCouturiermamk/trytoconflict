bd65dbedafe07ec6adfc236f75218d7697f1ce33
Anthony <Anthony@edu.mamk.fi>
2016-09-30 14:11:28 +0300

    first changes ...

--- src/WAConflict/Startup.cs
+++ src/WAConflict/Startup.cs
@@ -47,14 +47,17 @@ namespace WAConflict
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseStaticFiles();
+
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
         }
     }
 }
